0a48be985b84fb031d017b2a56a7268ebbab4b5a
vitoria-b <71644689+vitoria-b@users.noreply.github.com>
2020-10-05 09:02:47 -0300

    cor, clock, evento

--- jogoteste.py
+++ jogoteste.py
@@ -1,5 +1,18 @@
 import pygame
 def main():
+    #as definições dos objetos (variaveis)
     pygame.init()
-    pygame.display.set_mode([300, 300])
-main()
+    tela = pygame.display.set_mode([300, 300])
+    pygame.display.set_caption("Iniciando o Pygame")
+    relogio = pygame.time.Clock() #isso fica dentro do main, consigo capturar info de atualização da tela, o tempo.
+    cor_branca = (255,255,255) #definindo cor
+    sair = False
+    while sair != True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sair = True
+        relogio.tick(27) #atualizando de 27 quadros por segundo
+        tela.fill(cor_branca) #trabalhando com o fundo/preenchimentl, valor hexadecimal vai dar essa cor
+        pygame.display.update() #atualização na tela  tempo todo
+    pygame.quit()
+main() 
