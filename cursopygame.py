07fa8ad4f9941fbde6f072d213e08fe282a710fb
Victor_J_L <victorjolou@gmail.com>
2020-10-10 17:10:20 -0300

    Feito os exemplos até a aula 27

--- cursopygame.py
+++ cursopygame.py
@@ -5,16 +5,34 @@ def main():
     tela = pygame.display.set_mode([300, 300])
     pygame.display.set_caption("Iniciando o pygame")
     relogio = pygame.time.Clock()
+   
+   #CORES
     cor_branca = (255,255,255)
     cor_azul = (16,118,148)
     cor_verde = (74,117,43)
     cor_amarela = (255,248,0)
+    cor_roxa = (106, 52, 114)
 
+   #SUPERFÍCIES
     sup = pygame.Surface((200,200))
     sup.fill(cor_verde)
     sup2 = pygame.Surface((100,100))
     sup2.fill(cor_azul)
+
+   #OBJETOS 
     ret= pygame.Rect(10, 10, 45, 45)
+    ret2= pygame.Rect(100,80,130,60)
+
+   #FONTE
+    pygame.font.init()
+
+    font_padrao = pygame.font.get_default_font()
+    font_lose = pygame.font.SysFont(font_padrao, 45)
+    font_ganhou = pygame.font.SysFont(font_padrao, 30)
+
+   #ÁUDIOS 
+    audio_explosao = pygame.mixer.Sound('explosao.ogg')
+
     sair = False
 
     while sair != True:
@@ -42,10 +60,19 @@ def main():
         tela.fill(cor_branca)
         tela.blit(sup,[50,50])  
         tela.blit(sup2,[100,100])
+
+        (xant, yant) = (ret.left, ret.top)
         (ret.left, ret.top) = pygame.mouse.get_pos()
         ret.left -= ret.width/2
         ret.top -= ret.height/2
-        pygame.draw.rect(tela, cor_amarela, ret) 
+        if ret.colliderect(ret2):
+            text = font_lose.render('COLIDIU', 1, (255, 255, 255))
+            tela.blit(text, (150,150))
+            audio_explosao.play()
+            audio_explosao.set_volume(0.01)
+            (ret.left, ret.top) = (xant, yant)
+        pygame.draw.rect(tela, cor_amarela, ret)
+        pygame.draw.rect(tela, cor_roxa, ret2) 
         pygame.display.update()
     pygame.quit() 
 
