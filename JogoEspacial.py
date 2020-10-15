7973650921d5c3a02c44feb27e042d2a5d10e6ce
Victor_J_L <victorjolou@gmail.com>
2020-10-15 00:26:38 -0300

    Ajustes ate a aula 48

--- JogoEspacial.py
+++ JogoEspacial.py
@@ -1,5 +1,33 @@
 #Jogo Espacial 
 import pygame
+import random
+
+class Ret(object):
+    def __init__(self, numeroinicial):
+        self.lista = []
+        for x in range(numeroinicial):
+            leftrandom = random.randrange(2, 560)
+            toprandom = random.randrange(-580, -10)
+            width = random.randrange(10, 30)
+            height = random.randrange(15, 30)
+            self.lista.append(pygame.Rect(leftrandom, toprandom, width, height))
+
+    def mover(self):
+        for retangulo in self.lista:
+            retangulo.move_ip(0, 2)
+    
+    def cor(self, superficie):
+        for retangulo in self.lista:
+            pygame.draw.rect(superficie, (165, 214, 254), retangulo)
+    
+    def recriar(self):
+         for x in range(len(self.lista)):
+            if self.lista[x].top > 481:
+                leftrandom = random.randrange(2, 560)
+                toprandom = random.randrange(-580, -10)
+                width = random.randrange(10, 30)
+                height = random.randrange(15, 30)
+                self.lista[x] = (pygame.Rect(leftrandom, toprandom, width, height))
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, imagem):
@@ -31,6 +59,9 @@ def main ():
     img_nave = pygame.image.load("nave.png").convert_alpha()
     jogador = Player(img_nave)
     fundo = pygame.image.load("fundo.png").convert_alpha()
+
+    #Objetos
+    ret= Ret(30)
 
 
     while sair != True:
@@ -74,8 +105,10 @@ def main ():
 
                 
         relogio.tick(50)
-
         tela.blit(fundo, (0, 0))
+        ret.mover()
+        ret.cor(tela)
+        ret.recriar()
         jogador.update(tela)
         jogador.mover(vx,vy)
         
