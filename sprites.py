3d24d50b2a68964b574fee3ef16adb8312603da7
vitoria-b <71644689+vitoria-b@users.noreply.github.com>
2020-11-16 09:27:49 -0300

    adicionado design, só falta movimento

--- sprites.py
+++ sprites.py
@@ -371,11 +371,11 @@ iconef3grupo.add(iconef3)
 botaot3 = Botaot()
 transicao3grupo.add(botaot3)
 
-interromper = Inimigos3(5, 2000, "Imagens/Segunda Fase/UNIÃO.png") #dai quando clica no botao muda o y pro lugar certo deles"
-falarmal = Inimigos3(450, 2000, "Imagens/Segunda Fase/UNIÃO.png")
-gritar = Inimigos3(5, 2000, "Imagens/Segunda Fase/UNIÃO.png")
-desrespeitar = Inimigos3(450, 2000, "Imagens/Segunda Fase/UNIÃO.png")
-mentefechada  = Inimigos3(5, 2000, "Imagens/Segunda Fase/UNIÃO.png")
+interromper = Inimigos3(5, 2000, "Imagens/Terceira Fase/interromper.png") #dai quando clica no botao muda o y pro lugar certo deles"
+falarmal = Inimigos3(450, 2000, "Imagens/Terceira Fase/falarmal.png")
+gritar = Inimigos3(5, 2000, "Imagens/Terceira Fase/gritar.png")
+desrespeitar = Inimigos3(450, 2000, "Imagens/Terceira Fase/desrespeitar.png")
+mentefechada  = Inimigos3(5, 2000, "Imagens/Terceira Fase/mentefechada.png")
 inimigos3grupo.add(interromper, falarmal, gritar, desrespeitar, mentefechada)
 
 #Lista de Plataformas
